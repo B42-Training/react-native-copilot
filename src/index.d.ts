ed879c56b262f61a8994c1e44de1cccc136e688a
Jonas Stangl <jonas.stangl@b-42.com>
2021-01-20 19:35:08 +0100

    added arrowColor

--- src/index.d.ts
+++ src/index.d.ts
@@ -46,6 +46,7 @@ declare module 'react-native-copilot' {
     stopOnOutsideClick?: boolean; // Whether the tutorial should stop after clicking outside the step component
     labels: Labels;
     tooltipStyle: Object;
+    arrowColor: string;
   };
 
   /**
